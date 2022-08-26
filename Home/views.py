4fa39843a30f37f5f0867064d36aa2a75b72446b
Khoe-TV <cltbkha01@gmail.com>
2022-08-26 10:41:14 +0700

    first commit

--- Home/views.py
+++ Home/views.py
@@ -7,4 +7,5 @@ def index (request):
     response.writelines('<h1>Le Thanh Khoe</h1>')
     response.write('Day la app Home')
     response.write('<h2>Second update</h2>')
+    response.write('<h2>Third update</h2>')
     return response
